6a3bdd0a77783cc59cd601c0906ee73861e2ebb8
Flying-Tiantian <tianxuejin2014@hotmail.com>
2019-07-30 17:20:29 +0800

    .

--- models/__init__.py
+++ models/__init__.py
@@ -4,5 +4,7 @@ from .Mobilenet import Mobilenet_model, Mobilenet_160_model, Mobilenet_64_model
 from .Resnet import Resnet18_model, Resnet20_model, Resnet56_model, Resnet56_64_model, Resnet20_128_model
 from .Vgg import VGGa_model, VGG16_model, VGG19_model
 from .Inception import Inception_model
+from .Shufflenet import ShuffleNet_model
+from .Squeezenet import Squeezenet_model
 
-__all__ = ['Inception_model', 'Mobilenet_64_model', 'Mobilenet_160_model', 'LeNet_model', 'Mobilenet_model', 'Resnet18_model', 'Resnet20_model', 'Resnet20_128_model', 'Resnet56_model', 'Resnet56_64_model', 'VGGa_model', 'VGG16_model', 'VGG19_model']
+__all__ = ['ShuffleNet_model', 'Squeezenet_model', 'Inception_model', 'Mobilenet_64_model', 'Mobilenet_160_model', 'LeNet_model', 'Mobilenet_model', 'Resnet18_model', 'Resnet20_model', 'Resnet20_128_model', 'Resnet56_model', 'Resnet56_64_model', 'VGGa_model', 'VGG16_model', 'VGG19_model']
